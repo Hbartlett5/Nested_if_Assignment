6eedf76470a4e76b13b3240b333ad9af204a29d3
Hbartlett5 <hbcollege5@gmail.com>
2025-01-12 00:30:40 -0500

    fixed Question 1 Task 1

--- Nested_Decisions_The_Adventure_Game.py
+++ Nested_Decisions_The_Adventure_Game.py
@@ -1,16 +1,16 @@
 # Question 1: Nested Decisions: The Adventure Game
 
 # Task 1: Code Correction
-#place = input("Choose a place: forest or cave? ")
+place = input("Choose a place: forest or cave? ")
 
-#if place == "forest":
-    #action = input("climb a tree or cross a river?")
-    #if action == "climb a tree":
-        #print("You found a bird's nest!")
-    #elif action == "cross a river":
-        #print("You found a boat!")
-#elif place == "cave":
-    #print("You find a hidden treasure!")
+if place == "forest":
+    action = input("climb a tree or cross a river?")
+    if action == "climb a tree":
+        print("You found a bird's nest!")
+    elif action == "cross a river":
+        print("You found a boat!")
+elif place == "cave":
+    print("You find a hidden treasure!")
 
 # Task 2: Setting the Scene
 
